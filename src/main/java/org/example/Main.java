e3ac80cb6a4b46465a10e37bf37bd254428a8b16
SabrinaPrestes <sabrinaprestes25@gmail.com>
2025-06-04 15:31:00 -0300

    feat: adicionado entrada de dados pela classe Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+import org.example.calculator.Calculator;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calculator = new Calculator();
+
+        int sum = calculator.sum(7,3);
+        int sub = calculator.sub(5,3);
+        int multiply = calculator.multply(3,3);
+        int divide = calculator.divide(8,2);
+
+        System.out.println("Sum result: " + sum);
+        System.out.println("Subtraction result: " + sub);
+        System.out.println("Multiplication result: " + multiply);
+        System.out.println("Division result: " + divide);
+
+        try {
+            calculator.divide(10, 0);
+        } catch (ArithmeticException e) {
+            System.out.println("Error while dividing: " + e.getMessage());
+        }
     }
 }
